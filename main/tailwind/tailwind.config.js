db76bdf5989a6584cc722e038e885f10f2809fd8
unknown <abdullahrefatmo@gmail.com>
2023-11-26 03:56:21 +0200

    front

--- main/tailwind/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./ecommerce/*.html", "./assets/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailgrids/plugin")],
-}
-
